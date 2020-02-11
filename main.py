74218048cc36479942cd6271b351e74a8aba5afb
[Students] Toms Pilags <83DTPilags@rvt.lv>
2020-02-11 13:59:41 +0200

    added new design files

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, json, jsonify, render_template, request
+
+
+app = Flask('app')
+
+
+@app.route('/')
+def index_lapa():
+  return render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True, port=5000, debug=True)
